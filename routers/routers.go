d311577797094775457c096f2c285fe7e2f8dc5e
“Zaldins” <saldinata.bobby@gmail.com>
2023-12-26 00:23:25 +0700

    remove slash add wallet

--- routers/routers.go
+++ routers/routers.go
@@ -210,7 +210,7 @@ func API(router *gin.RouterGroup, db *gorm.DB) {
 
 		walletGroup := v1group.Group("/wallets", tokenSignature(), accountType(), betaVersion())
 		{
-			walletGroup.POST("/", wallet.Add)
+			walletGroup.POST("", wallet.Add)
 			walletGroup.GET("/", wallet.List)
 			walletGroup.PATCH("/amount/:id-wallet", wallet.UpdateAmount)
 		}
