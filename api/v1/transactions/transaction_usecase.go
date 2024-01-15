dac2d1bcd84be4a609a4c5325d920faf00d66e5d
“Zaldins” <saldinata.bobby@gmail.com>
2024-01-15 12:30:41 +0700

    add transaction travel

--- api/v1/transactions/transaction_usecase.go
+++ api/v1/transactions/transaction_usecase.go
@@ -192,7 +192,7 @@ func (s *TransactionUseCase) Add(ctx *gin.Context, request *dtos.TransactionRequ
 		Credit = float64(request.Amount)
 	}
 
-	if request.IDMasterExpenseCategories != "" {
+	if request.IDMasterExpenseCategories != "" && request.IDTravel == "" {
 
 		// if balance is insufficient
 		if dataLastBalance.Balance < float64(request.Amount) {
